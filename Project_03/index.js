cf5b590efc4675336387c1d048b8c288c30515f6
Subha822-hub <suseraj1974@gmail.com>
2024-01-03 09:46:13 +0530

    Changes For Animation

--- Project_03/index.js
+++ Project_03/index.js
@@ -11,38 +11,127 @@ function drumKit(e)
     if(eventkey!=null)
     {
         var word = eventkey;
+        var b = document.querySelectorAll("."+eventkey)[0];
+
     }
     else
     {
         var word = this.innerHTML;
+        var b = this;
     }
     switch (word) {
         case "w":
             var audio = new Audio('sounds/crash.mp3');
+            if(this.classList!=undefined)
+            {
+                var x =this.classList.add("pressed");
+            }
+            else
+            {
+                var x =b.classList.add("pressed");
+            }
+
+            setTimeout(function() 
+            {
+                b.classList.remove("pressed");
+            }, 100);
+           
             audio.play();
             break;
         case "a":
             var audio = new Audio('sounds/kick-bass.mp3');
+            if(this.classList!=undefined)
+            {
+                var x =this.classList.add("pressed");
+            }
+            else
+            {
+                var x =b.classList.add("pressed");
+            }
+            setTimeout(function() 
+            {
+                b.classList.remove("pressed");
+            }, 100);
             audio.play();
             break;
         case "s":
-            var audio = new Audio('sounds/snare.mp3')
+            var audio = new Audio('sounds/snare.mp3');
+            if(this.classList!=undefined)
+            {
+                var x =this.classList.add("pressed");
+            }
+            else
+            {
+                var x =b.classList.add("pressed");
+            }
+            setTimeout(function() 
+            {
+                b.classList.remove("pressed");
+            }, 100);
             audio.play();
             break;
         case "d":
             var audio = new Audio('sounds/tom-1.mp3')
+            if(this.classList!=undefined)
+            {
+                var x =this.classList.add("pressed");
+            }
+            else
+            {
+                var x =b.classList.add("pressed");
+            }
+            setTimeout(function() 
+            {
+                b.classList.remove("pressed");
+            }, 100);
             audio.play();
             break;
         case "j":
             var audio = new Audio('sounds/tom-2.mp3')
+            if(this.classList!=undefined)
+            {
+                var x =this.classList.add("pressed");
+            }
+            else
+            {
+                var x =b.classList.add("pressed");
+            }
+            setTimeout(function() 
+            {
+                b.classList.remove("pressed");
+            }, 100);
             audio.play();
             break;
         case "k":
             var audio = new Audio('sounds/tom-3.mp3')
+            if(this.classList!=undefined)
+            {
+                var x =this.classList.add("pressed");
+            }
+            else
+            {
+                var x =b.classList.add("pressed");
+            }
+            setTimeout(function() 
+            {
+                b.classList.remove("pressed");
+            }, 100);
             audio.play();
             break;
         case "l":
             var audio = new Audio('sounds/tom-4.mp3')
+            if(this.classList!=undefined)
+            {
+                var x =this.classList.add("pressed");
+            }
+            else
+            {
+                var x =b.classList.add("pressed");
+            }
+            setTimeout(function() 
+            {
+                b.classList.remove("pressed");
+            }, 100);
             audio.play();
             break;
     
